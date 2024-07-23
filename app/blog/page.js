8db8e2db9a77b63a02c20bb460a4e85b2cc16bf6
alisohail2448 <alisohail2448@gmail.com>
2024-07-23 16:04:51 +0530

    Sohail | Feat: Created my portfolio for Full Stack Developer

--- app/blog/page.js
+++ app/blog/page.js
@@ -4,7 +4,7 @@ import { personalData } from "@/utils/data/personal-data";
 import BlogCard from "../components/homepage/blog/blog-card";
 
 async function getBlogs() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
+  const res = await fetch(`https://dev.to/api/articles`)
 
   if (!res.ok) {
     throw new Error('Failed to fetch data')
@@ -20,7 +20,7 @@ async function page() {
   return (
     <div className="py-8">
       <div className="flex justify-center my-5 lg:py-8">
-        <div className="flex  items-center">
+        <div className="flex items-center">
           <span className="w-24 h-[2px] bg-[#1a1443]"></span>
           <span className="bg-[#1a1443] w-fit text-white p-2 px-5 text-2xl rounded-md">
             All Blog
@@ -29,7 +29,7 @@ async function page() {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 md:gap-5 lg:gap-8 xl:gap-10">
+      <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 md:gap-5 lg:gap-8 xl:gap-10">
         {
           blogs.map((blog, i) => (
             blog?.cover_image &&
